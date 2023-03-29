b633c14898f23a7d79265863901b61fd61bae3e4
sakuexe <saku.karttunen@gmail.com>
2023-03-29 10:17:40 +0300

    removed unnecessary lines from colorscheme.lua

--- lua/sakuexe/core/colorscheme.lua
+++ lua/sakuexe/core/colorscheme.lua
@@ -4,12 +4,6 @@ if not status then
 	return
 end
 
-local status_transparent, _ = pcall(vim.cmd, "TransparentEnable")
-if status_transparent then
-	print("Running :TransparentEnable")
-	return
-end
-
 if vim.fn.has("termguicolors") then
 	vim.opt.termguicolors = true
 end
